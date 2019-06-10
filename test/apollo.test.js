df6c600553ef05fcd479e6cc4a94d219f2f13e1f
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-10 18:55:55 +0800

    add test case: apollo disabled

--- test/apollo.test.js
+++ test/apollo.test.js
@@ -4,25 +4,44 @@ const mock = require('egg-mock');
 const assert = require('assert');
 
 describe('test/apollo.test.js', () => {
-  let app;
-  before(() => {
-    app = mock.app({
-      baseDir: 'apps/apollo-test',
+  describe('apollo-test', () => {
+    let app;
+    before(() => {
+      app = mock.app({
+        baseDir: 'apps/apollo-test',
+      });
+      return app.ready();
     });
-    return app.ready();
+
+    after(() => app.close());
+    afterEach(mock.restore);
+
+    it('should GET /', () => {
+    // [!important] 执行测试前请前确保apollo中真实存在这些配置
+      assert(app.config.deepexi.greeting === 'hi, guys!');
+      assert(app.config.foo === 'bar');
+
+      return app.httpRequest()
+        .get('/')
+        .expect('hi, apollo')
+        .expect(200);
+    });
   });
 
-  after(() => app.close());
-  afterEach(mock.restore);
+  describe('disabled', () => {
+    let app;
+    before(() => {
+      app = mock.app({
+        baseDir: 'apps/disabled',
+      });
+      return app.ready();
+    });
 
-  it('should GET /', () => {
-    // [!important] 执行测试前请前确保apollo中真实存在这些配置
-    assert(app.config.deepexi.greeting === 'hi, guys!');
-    assert(app.config.foo === 'bar');
+    after(() => app.close());
+    afterEach(mock.restore);
 
-    return app.httpRequest()
-      .get('/')
-      .expect('hi, apollo')
-      .expect(200);
+    it('should undefined app.apollo', () => {
+      assert(!app.apollo);
+    });
   });
 });
