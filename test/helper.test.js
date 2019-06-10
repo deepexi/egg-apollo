acbf8383b01e52c84339a1a4f81ca355f74f38de
廖锦锋 <liaojinfeng@liaojinfengs-MacBook-Pro.local>
2019-06-10 20:32:13 +0800

    add test case for mergeConfigs()

--- test/helper.test.js
+++ test/helper.test.js
@@ -5,6 +5,19 @@ const assert = require('assert');
 
 
 describe('test/helper.test.js', () => {
+  describe('mergeConfigs()', () => {
+    it('should right', () => {
+      const merged = helper.mergeConfigs([
+        { a: 'a', b: 'b' },
+        { a: 'a1', b: 'b1' },
+        { b: 'b2', 'c.d': 'cd' },
+      ]);
+      assert(merged.a === 'a1');
+      assert(merged.b === 'b2');
+      assert(merged['c.d'] === 'cd');
+    });
+  });
+
   describe('parseToJson()', () => {
     it('should right', () => {
       const json = helper.parseToJson({
